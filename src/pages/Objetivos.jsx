2757e54173b96db26bf0c90a1cda5653b521f22a
EdwardMelendezM <106676335+EdwardMelendezM@users.noreply.github.com>
2023-02-07 11:54:12 -0500

    Implementado objetivos y estilos

--- src/pages/Objetivos.jsx
+++ src/pages/Objetivos.jsx
@@ -1,5 +1,26 @@
+import "./Objetivos.css";
 const Objetivos = () => {
-  return <h3>Nosotros</h3>;
+  return (
+    <main className="mainObjetivos">
+      <div className="row">
+        <div className="col">
+          <h3>¿A quién va dirigido?</h3>
+          <p>Dirigido a estudiantes universitarios de la Unsaac</p>
+        </div>
+        <img src="alumnosJunta1.png" alt="Alumos reunion" />
+      </div>
+      <div className="row">
+        <img src="alumnosJunta2.png" alt="Alumnos reunion" />
+        <div className="col">
+          <h3>¿Que te proponemos?</h3>
+          <p>
+            Poner a disposicion, herramientas y recursos para fortalecer tu roll
+            y brindar acompñamiento socioefectivo y cognitivo a los estudiantes
+          </p>
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default Objetivos;
