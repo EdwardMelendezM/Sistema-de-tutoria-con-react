565521dc7591b33e91817a7f8ef8236cf61119cf
EdwardMelendezM <106676335+EdwardMelendezM@users.noreply.github.com>
2023-02-09 22:00:50 -0500

    Agregando panel de ayuda - falta terminar

--- src/pages/Ayuda.jsx
+++ src/pages/Ayuda.jsx
@@ -1,5 +1,51 @@
+import "./Ayuda.css";
 const Ayuda = () => {
-  return <h3>Ayuda</h3>;
+  return (
+    <main className="mainAyuda">
+      <div className="row">
+        <div className="article">
+          <div className="cardContext img1">
+            <h3>
+              <b>Lic.</b> Maria Maldonado Paredez
+            </h3>
+            <h2>
+              <i>Especialista en problemas familiares</i>
+            </h2>
+          </div>
+        </div>
+        <div className="article">
+          <div className="cardContext img2">
+            <h3>
+              <b>Lic.</b> Maria Maldonado Paredez
+            </h3>
+            <h2>
+              <i>Especialista en problemas familiares</i>
+            </h2>
+          </div>
+        </div>
+        <div className="article">
+          <div className="cardContext img3">
+            <h3>
+              <b>Lic.</b> Maria Maldonado Paredez
+            </h3>
+            <h2>
+              <i>Especialista en problemas familiares</i>
+            </h2>
+          </div>
+        </div>
+        <div className="article">
+          <div className="cardContext img4">
+            <h3>
+              <b>Lic.</b> Maria Maldonado Paredez
+            </h3>
+            <h2>
+              <i>Especialista en problemas familiares</i>
+            </h2>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default Ayuda;
