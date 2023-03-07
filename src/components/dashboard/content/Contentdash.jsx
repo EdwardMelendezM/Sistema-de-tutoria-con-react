7bdb3c3740352c00dfc28ec86e846df1adf0e99b
EdwardMelendezM <106676335+EdwardMelendezM@users.noreply.github.com>
2023-03-06 20:54:06 -0500

    Retoques de reuniones

--- src/components/dashboard/content/Contentdash.jsx
+++ src/components/dashboard/content/Contentdash.jsx
@@ -2,33 +2,43 @@ import "./contentdash.css";
 export default function Contentdash() {
   return (
     <div className="containerDashboar">
-      <h3 className="containerDashboar-titulo">Pendientes</h3>
-      <table className="containerDashboar_table">
-        <thead className="containerDashboar_table-head">
-          <tr>
-            <th>Empieza</th>
-            <th>Titulo</th>
-          </tr>
-        </thead>
-        <tbody className="containerDashboar_table-body">
-          <tr>
-            <td>2h</td>
-            <td>Session de aprendizaje</td>
-          </tr>
-          <tr>
-            <td>2 dias y 30min</td>
-            <td>Maestria de colmena</td>
-          </tr>
-          <tr>
-            <td>1 semana y 2 dias</td>
-            <td>Practica de artesania</td>
-          </tr>
-          <tr>
-            <td>1 semana y 3 dias</td>
-            <td>Practica de matematica</td>
-          </tr>
-        </tbody>
-      </table>
+      <h3 className="containerDashboar-titulo">Reuniones</h3>
+      <div className="containerDashboar_table-table">
+        <table className="containerDashboar_table">
+          <thead className="containerDashboar_table-head">
+            <tr>
+              <th>Empieza</th>
+              <th>Titulo</th>
+            </tr>
+          </thead>
+          <tbody className="containerDashboar_table-body">
+            <tr>
+              <td>2h</td>
+              <td>Session de aprendizaje</td>
+            </tr>
+            <tr>
+              <td>2 dias y 30min</td>
+              <td>Maestria de colmena</td>
+            </tr>
+            <tr>
+              <td>1 semana y 2 dias</td>
+              <td>Practica de artesania</td>
+            </tr>
+            <tr>
+              <td>1 semana y 2 dias</td>
+              <td>Practica de artesania</td>
+            </tr>
+            <tr>
+              <td>1 semana y 2 dias</td>
+              <td>Practica de artesania</td>
+            </tr>
+            <tr>
+              <td>1 semana y 2 dias</td>
+              <td>Practica de artesania</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
