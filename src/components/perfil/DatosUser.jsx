97c004d2e0a408dfd5c6be216456181fe3f219c6
EdwardMelendezM <106676335+EdwardMelendezM@users.noreply.github.com>
2023-03-07 21:14:37 -0500

    implementando toogle de edit datos

--- src/components/perfil/DatosUser.jsx
+++ src/components/perfil/DatosUser.jsx
@@ -1,5 +1,10 @@
+import { useState } from "react";
 import "./datosuser.css";
 export default function DatosUser() {
+  const [isEdit, setIsEdit] = useState(false);
+  const handleClickEdit = () => {
+    setIsEdit(!isEdit);
+  };
   return (
     <div className="containerDatosUser">
       <p className="containerDatosUser_p">Datos personales</p>
@@ -9,36 +14,66 @@ export default function DatosUser() {
             <ion-icon name="caret-forward"></ion-icon>
             Nombre completo:
           </p>
-          <p className="containerDatosUser_datos-input">
-            Nombre Completo de Estudiante
-          </p>
+          {isEdit ? (
+            <input
+              type="text"
+              className="containerDatosUser_datos-input_input"
+              value="Nombre Completo de Estudiante"
+            />
+          ) : (
+            <p className="containerDatosUser_datos-input">
+              Nombre Completo de Estudiante
+            </p>
+          )}
         </div>
         <div className="containerDatosUser_datos_personales">
           <p className="containerDatosUser_datos-input-label">
             <ion-icon name="caret-forward"></ion-icon>
             Correo electronico:
           </p>
-          <p className="containerDatosUser_datos-input">
-            user010101sss@email.com
-          </p>
+          {isEdit ? (
+            <input
+              type="text"
+              className="containerDatosUser_datos-input_input"
+              value="user010101sss@email.com"
+            />
+          ) : (
+            <p className="containerDatosUser_datos-input">
+              user010101sss@email.com
+            </p>
+          )}
         </div>
         <div className="containerDatosUser_datos_personales">
           <p className="containerDatosUser_datos-input-label">
             <ion-icon name="caret-forward"></ion-icon>
             Direccion:
           </p>
-          <p className="containerDatosUser_datos-input">
-            direccion av ambrella 101 Mz L
-          </p>
+          {isEdit ? (
+            <input
+              type="text"
+              className="containerDatosUser_datos-input_input"
+              value="Direccion av ambrella 101 Mz L"
+            />
+          ) : (
+            <p className="containerDatosUser_datos-input">
+              Direccion av ambrella 101 Mz L
+            </p>
+          )}
         </div>
         <div className="containerDatosUser_datos_personales">
           <p className="containerDatosUser_datos-input-label">
             <ion-icon name="caret-forward"></ion-icon>
             Numero de celular:
           </p>
-          <p className="containerDatosUser_datos-input">
-            Celular 944703265954184
-          </p>
+          {isEdit ? (
+            <input
+              type="text"
+              className="containerDatosUser_datos-input_input"
+              value="944703265954184"
+            />
+          ) : (
+            <p className="containerDatosUser_datos-input">944703265954184</p>
+          )}
         </div>
 
         <div className="containerDatosUser_datos_personales">
@@ -46,13 +81,34 @@ export default function DatosUser() {
             <ion-icon name="caret-forward"></ion-icon>
             Telefono:
           </p>
-          <p className="containerDatosUser_datos-input">
-            Celular 944703265954184
-          </p>
+
+          {isEdit ? (
+            <input
+              type="text"
+              className="containerDatosUser_datos-input_input"
+              value="944703265954184"
+            />
+          ) : (
+            <p className="containerDatosUser_datos-input">944703265954184</p>
+          )}
         </div>
       </div>
       <div className="containerDatosUser_button">
-        <button className="containerDatosUser_button-btn edit">Editar</button>
+        {isEdit ? (
+          <button
+            className="containerDatosUser_button-btn edit"
+            onClick={handleClickEdit}
+          >
+            Confirmar
+          </button>
+        ) : (
+          <button
+            className="containerDatosUser_button-btn edit"
+            onClick={handleClickEdit}
+          >
+            Editar
+          </button>
+        )}
         <button className="containerDatosUser_button-btn add">Info</button>
       </div>
     </div>
