ed606cb554f096c57ab48695ae379bf3040b8c90
EdwardMelendezM <106676335+EdwardMelendezM@users.noreply.github.com>
2023-03-07 22:45:09 -0500

    elaborando switch components

---
+++ src/components/switch/Switch.jsx
@@ -0,0 +1,8 @@
+export default function Switch() {
+  return (
+    <div className="switchContainer">
+      <input type="checkbox" id="switch" />
+      <label htmlFor="switch" className="labelSwitch"></label>
+    </div>
+  );
+}
