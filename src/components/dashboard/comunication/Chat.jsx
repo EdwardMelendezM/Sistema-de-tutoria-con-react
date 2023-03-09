d940d62b4fe803837e57c8c55c00131ba463d52b
EdwardMelendezM <106676335+EdwardMelendezM@users.noreply.github.com>
2023-03-09 13:29:28 -0500

    implementando user header - s

--- src/components/dashboard/comunication/Chat.jsx
+++ src/components/dashboard/comunication/Chat.jsx
@@ -1,29 +1,59 @@
+import "./chat.css";
 export default function Chat() {
   return (
     <div className="containerChat">
-      <div className="containerChat_User">
+      <div className="containerChat_Users">
         <button className="container_Chat_User_button">
-          <div className="icon"></div>
-          <p>Create New</p>
+          <ion-icon name="add-outline"></ion-icon>
+          <p className="container_Chat_User_button_text">Create New</p>
         </button>
         <div className="containerChat_User_Container">
+          <div className="containerChat_User_Container_title">
+            <p className="containerChat_User_Container_titulo_p">Chat</p>
+            <ion-icon name="ellipsis-horizontal-outline"></ion-icon>
+          </div>
           <div className="containerChat_User_Container_buscador">
             <input
               type="text"
               className="containerChat_User_Container_buscador_input"
+              placeholder="Search name"
             />
+            <ion-icon name="search-outline"></ion-icon>
           </div>
           <div className="containerChat_User_Container_Users">
             <div className="containerChat_User_Container_Users_user">
               <div className="containerChat_User_Container_Users_user_img">
-                <img src="" alt="user" />
+                <img
+                  src="userImage.jpg"
+                  alt="user"
+                  className="containerChat_User_Container_Users_user_img"
+                />
+                <ion-icon name="ellipse-outline"></ion-icon>
               </div>
               <div className="containerChat_User_Container_Users_user_text">
                 <p className="containerChat_User_Container_Users_user_text_name">
-                  Albeto Fujimori
+                  Alberto Fujimori
                 </p>
-                <p className="containerChat_User_Container_Users_user_text_">
-                  Albeto Fujimori
+                <p className="containerChat_User_Container_Users_user_text_fecha">
+                  Hour ago
+                </p>
+              </div>
+            </div>
+            <div className="containerChat_User_Container_Users_user">
+              <div className="containerChat_User_Container_Users_user_img">
+                <img
+                  src="userImage.jpg"
+                  alt="user"
+                  className="containerChat_User_Container_Users_user_img"
+                />
+                <ion-icon name="ellipse-outline"></ion-icon>
+              </div>
+              <div className="containerChat_User_Container_Users_user_text">
+                <p className="containerChat_User_Container_Users_user_text_name">
+                  Alberto Fujimori
+                </p>
+                <p className="containerChat_User_Container_Users_user_text_fecha">
+                  Hour ago
                 </p>
               </div>
             </div>
@@ -33,15 +63,19 @@ export default function Chat() {
       <div className="containerChat_Messages">
         <div className="containerChat_Messages_header">
           <div className="containerChat_Messages_header_content">
-            <img src="" alt="" />
+            <img
+              src="userImage.jpg"
+              alt="user"
+              className="containerChat_Messages_header_content_img"
+            />
             <div className="containerChat_Messages_header_user">
               <p className="containerChat_Messages_header_user_name">Melonie</p>
               <p className="containerChat_Messages_header_user_online">
                 Online
               </p>
             </div>
-            <div className="containerChat_Messages_header_content_icon"></div>
           </div>
+          <ion-icon name="ellipsis-horizontal-outline"></ion-icon>
         </div>
         <div className="containerChat_Messages_body">
           <div className="containerChat_Messages_body_message">
--- src/pages/computo/session/ComputoSession.jsx
+++ src/pages/computo/session/ComputoSession.jsx
@@ -1,7 +1,10 @@
+import Chat from "../../../components/dashboard/comunication/Chat";
+
 export default function ComputoSession() {
   return (
     <div>
       <h3>Computo Session</h3>
+      <Chat />
     </div>
   );
 }
